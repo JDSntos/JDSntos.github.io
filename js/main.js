9556afe09618dad0e598b22f4358dea1b2e6c4ac
Jimwell Delos Santos <39312440+JDSntos@users.noreply.github.com>
2021-09-07 20:46:25 +0800

    Fix Scrolling
    Fix scrolling to smooth

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.sidenav #navList > .nav-item').click(function (e) {
-        $('.sidenav #navList > .nav-item').removeClass('active');
-        $(this).addClass('active');
-    });
-});
